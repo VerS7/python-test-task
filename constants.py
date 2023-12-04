bf2797864567498d7a3359abb23d9bb08d9af106
Davydov Albert <103847309+VerS7@users.noreply.github.com>
2023-12-04 17:08:41 +0300

    base app constants

--- constants.py
+++ constants.py
@@ -1,3 +1,21 @@
+"""
+Константы приложения
+"""
+from os import getenv
 
+try:
+    import dotenv
+    dotenv.load_dotenv(".env")
+except ImportError:
+    pass
 
+# Telegram API
+API_ID = getenv("API_ID")
+API_HASH = getenv("API_HASH")
 
+# DB
+DB_HOST = getenv("DB_HOST")
+DB_PORT = getenv("DB_PORT")
+DB_SECRET = getenv("DB_SECRET")
+DB_USERNAME = getenv("DB_USERNAME")
+DB_NAME = getenv("DB_NAME")
